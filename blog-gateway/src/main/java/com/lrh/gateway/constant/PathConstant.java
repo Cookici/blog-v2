6d80b74cebf0100ee0950b971551dd135c02a8e8
ranran <ranran@ant3r.com>
2025-01-18 21:25:53 +0800

    feat: 配置nacos账号和密码在运行机器上

---
+++ blog-gateway/src/main/java/com/lrh/gateway/constant/PathConstant.java
@@ -0,0 +1,14 @@
+package com.lrh.gateway.constant;
+
+/**
+ * @ProjectName: blog-v2
+ * @Package: com.lrh.common.constant
+ * @ClassName: PathConstant
+ * @Author: 63283
+ * @Description:
+ * @Date: 2025/1/18 16:50
+ */
+
+public class PathConstant {
+    public static final String NACOS_CONFIG_PATH = "C:\\Users\\63283\\nacos.json";
+}
---
+++ blog-oss/src/main/java/com/lrh/oss/config/NacosConfig.java
@@ -0,0 +1,23 @@
+package com.lrh.oss.config;
+
+import com.lrh.common.nacos.NacosBeanAutowire;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ProjectName: blog-v2
+ * @Package: com.lrh.article.infrastructure.config
+ * @ClassName: NacosConfig
+ * @Author: 63283
+ * @Description:
+ * @Date: 2025/1/18 17:03
+ */
+@Configuration
+public class NacosConfig {
+
+    @Bean
+    public NacosBeanAutowire nacosBeanAutowire() {
+        return new NacosBeanAutowire();
+    }
+
+}
